3c1ae00cf122209c6ab77256453395fc162c5341
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2019-03-22 14:02:56 +0100

    pokemon.php has been finished

--- Pokemon.php
+++ Pokemon.php
@@ -18,7 +18,25 @@ class ClassName
 	{
 		$notification = null;
 		if (isset($this->weakness[$regular])) {
-			$health += $this->weakness[$regular]
+			$health += $this->weakness[$regular];
+			$notification = 'It\'s very effective!';
+		}
+		if (isset($this->resistance[$regular])) {
+			$health += $this->resistance[$regular];
+			$notification = 'It\'s not very effective...';
+		}
+		if ($health <= 0) {
+			$health = 0;
+			return 'The attack had no effect!';
+		}
+		$this->hp -= $health;
+		if ($this->hp <= 0) {
+			return $this->nickname. 'has been defeated';
+		}
+		if ($notification = null) {
+			return $notification;
+		}else{
+			return $this->nickname. ' has ' .$this->hp;
 		}
 	}
 }

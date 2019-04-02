27c109d9759fd977bf4b41d63cd960384cf9ba76
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2019-04-02 11:40:16 +0200

    added comments

--- Pokemon.php
+++ Pokemon.php
@@ -16,19 +16,24 @@ class Pokemon
 
 	public function GetAttacked($regular, $health)
 	{
+		//set notification to null
 		$notification = null;
+		//if the attack is of the type you're weak to, set notification to "it's very effective"
 		if (isset($this->weakness[$regular])) {
 			$health += $this->weakness[$regular];
 			$notification = 'It\'s very effective!';
 		}
+		//if the attack is of the type you're resistant to, set notification to "it's not very effective"
 		if (isset($this->resistance[$regular])) {
 			$health += $this->resistance[$regular];
 			$notification = 'It\'s not very effective...';
 		}
+		//if your health is at zero or less, say "the attack had no effect"
 		if ($health <= 0) {
 			$health = 0;
 			return 'The attack had no effect!';
 		}
+		//if the health gets to 0, say that the current pokemon is defeated
 		$this->hp -= $health;
 		if ($this->hp <= 0) {
 			return $this->nickname. 'has been defeated';
